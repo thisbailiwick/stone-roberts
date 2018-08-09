37e81ae43d68adff88f4d2a81e95abb7d75e7035
Kevin Clark <kevin@thisbailiwick.com>
2018-08-09 16:03:30 -0700

    break cache

--- web/app/themes/stone-roberts-anew/app/setup.php
+++ web/app/themes/stone-roberts-anew/app/setup.php
@@ -11,8 +11,8 @@
 	* Theme assets
 	*/
  add_action('wp_enqueue_scripts', function () {
-	wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, '1.3');
-	wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], '1.4', true);
+	wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, '1.4');
+	wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], '1.5', true);
  }, 100);
 
  /**
